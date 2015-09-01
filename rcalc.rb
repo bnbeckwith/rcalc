ad2154feb6a6ef63f6c5341bc4c76d5120e63309
Benjamin Beckwith <bnbeckwith@gmail.com>
2015-08-31 21:58:45 -0400

    Enable positioning of cursor on input error
    When detecting a problem with the input, print an error message, show
    the troublesome line and position the cursor at the problem spot.

--- rcalc.rb
+++ rcalc.rb
@@ -4,42 +4,57 @@ require 'readline'
 
 $version = "1.0"
 
+# Thow an exception with the troublesome token number attached
+class CalcError < StandardError
+  attr_reader :token_index
+  attr_reader :message
+
+  def initialize(token_index, message)
+    @token_index = token_index
+    @message     = message
+  end
+end
+
+
 # Evaluate the given tokens
 def eval_stack(tokens)
-  stack = []
-  while tokens.length > 0 
-    token = tokens.shift
+  tokens.each_with_index.reduce([]) { |stack, (token, index)|
+    # If the token is a string, assume an operator
     if token.class == String
       operand = stack.pop()
       begin
         arity = operand.method(token).arity
-      rescue Exception => e
-        return e.message
+      rescue NameError => e
+        raise CalcError.new(index, e.message)
       end
       begin
         res = operand.send(token,*stack.slice!(-arity,arity))
-      rescue Exception => e
-        return e.message
+      rescue ArgumentError => e
+        raise CalcError.new(index, e.message)
       end
       stack.push(res)
     else
       stack.push(token)
     end
-  end
-  stack
-end
+  }
+ end
 
 # Change input the looks like numbers into actual numbers
 def parse_numbers(stack)
-  stack.map { |tok|
+  stack.map.each_with_index { |tok, idx|
     if /[+-]?[0-9]/ =~ tok
-      eval(tok)
+      begin
+        eval(tok)
+      rescue SyntaxError => e
+        raise CalcError.new(idx, e.message)
+      end
     else
       tok
     end
   }
 end 
 
+# Process a given input line and return the result
 def calculate_line(line)
 
   # tokenize line
@@ -52,6 +67,13 @@ def calculate_line(line)
   # Evaluate the stack
   eval_stack(stack)
 
+end
+
+# Find the nth token
+def token_start(line,idx)
+  (0...idx).reduce(0) { |pos, i|
+    line.index(" ",pos+1) || 0
+  }
 end
 
 # Setup some options for the user
@@ -78,6 +100,21 @@ else
   # Print header for interactive use
   puts "RCalc version %s" % $version
 
+  error = nil
+  t_idx = nil
+  # Proc for handling errors
+  Readline.pre_input_hook = Proc.new do
+    if error 
+      Readline.insert_text( error )
+      if t_idx
+        Readline.point = token_start(error,t_idx)+1
+        t_idx = nil
+      end
+      error = nil
+      Readline.redisplay()
+    end
+  end
+  
   # The main part of the program
   while buf = Readline.readline("> ", true)
     
@@ -88,15 +125,19 @@ else
 
     begin
       result = calculate_line(buf)
-    rescue Exception => e
+    rescue CalcError => e
       puts e.message
-      next
+      error = buf
+      t_idx = e.token_index
     end
     
-    if result.length > 1
+    if result and result.length > 1
       puts "Error: Not enough operators"
+      error = buf
+    else
+      puts result
     end
-    puts result
+    
   end
 end
 
